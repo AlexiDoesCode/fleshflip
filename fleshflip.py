27d89a5818fb5201913f2dda7078f9dc8e02d04b
AlexiDoesCode <30761946+AlexiDoesCode@users.noreply.github.com>
2019-10-27 16:03:42 +0800

    Added working investment system + working HC
    Instead of getting 2x per round, you now get 1.97x of your bet per round. The 0.03x goes to the house.

--- fleshflip.py
+++ fleshflip.py
@@ -1,28 +1,68 @@
 import random
 import sys
 
+#coinflip
 bet = 0
+
+#balances
 balance = 1000
 hcbalance = 0
+
+#HC
 hcamount = 0
 maxHC = 10000
 currentHC = 0
+hcShare = 0
+#bankroll
+bankroll = 1000000
+maxBet = bankroll * 0.75/100
+
+#investment
+investmentAmount = 0
+investedAmount = 0
+percentOfStake = investedAmount/bankroll
+gameProfit = 0
+investProfit = 0
+
+#game
 win = False
 mode = None
 game_started = True
 
 
+#loops
+loop = True
+
+
+print("Welcome to Flesh Flip")
+print("Show the list of commands by inputting 'help'")
+
 while game_started == True:
         
-        #STARTUP
-        
-        while mode not in ("heads", "tails", "balance", "faucet", "hcbuy", "give","exit"):
-            mode = input("What side do you think would land? Input 'heads' or 'tails', Do 'balance' to see your balance, 'faucet' to receive 1000 bits, 'hcbuy' to buy house commission shares  ")
-
+        #STARTUP   
+        while mode not in ("heads", "tails", "balance", "faucet", "hcbuy", "give","exit", "invest", "stats","help","stake"):
+                mode = input("> ")
+        if mode == "help":
+                print("Commands: ")
+                print("heads - chooses the heads side")
+                print("tails - chooses the tails side")
+                print("balance - shows your balance, and amount of owned shares")
+                print("faucet - get free 1000 bits added to your balance")
+                print("invest - invest in to the Fleshflip Bankroll")
+                print("stats - show the stats")
+                print("help - show this page")
+                print("stake - shows the stake of your investment")
+                mode = None
+        try:
+                if (int(bet)> int(maxBet)):
+                                print("You cannot bet more than ",maxBet," bits")
+                                mode = None
+        except(ValueError):
+                print("")
         try:
                 #CHOOSE YOUR BET AMOUNT
                 if mode == "tails" or mode == "heads":
-                    bet = input("What is your bet?")
+                    bet = input("What is your bet? ")
                 else:
                     print("")
         except(ValueError):
@@ -39,6 +79,9 @@ while game_started == True:
                         if(int(bet) > int(balance)):
                                     print ("Not enough balance")
                                     mode = None
+                        else:
+                                balance = int(balance) - int(bet)
+
         except(ValueError):
                 print("Refrain from using letters, or other symbols, when asked for a numerical value.")
                 
@@ -58,41 +101,74 @@ while game_started == True:
                     if highorlow > 50:
                         print("The coin flipped Heads") 
                         print("You win!")
-                        balance = int(balance)+ int(bet)*2
+                        balance = int(balance)+ int(bet)*1.97
+                        bankroll = int(bankroll) - int(bet)
+                        gameProfit = int(bet) * float(percentOfStake)
+                        investedAmount = int(investedAmount) - int(gameProfit)
+                        investProfit = investProfit - gameProfit
+                        gameProfit = 0
+                        hcShare = int(bet) * 0.03
+                        if (int(hcamount) > 0):
+                                balance = int(balance)+ int(hcShare) * float(int(hcamount)/int(maxHC))
                         mode = None
                     else:
                         print ("The coin flipped Tails")
                         print("You lose!")
+                        investProfit = investProfit + gameProfit
+                        bankroll = int(bankroll) + int(bet)
                         balance = int(balance) - int(bet)
+                        gameProfit = int(bet) * float(percentOfStake)
+                        investedAmount = int(investedAmount) + int(gameProfit)
+                        investProfit = investProfit + gameProfit
+                        gameProfit = 0
+                        hcShare = int(bet) * 0.03
+                        if (int(hcamount) > 0):
+                                balance = int(balance)+ int(hcShare) * float(int(hcamount)/int(maxHC))
                         mode = None
                 if mode == "tails":
                     if highorlow > 50:
                         print("The coin flipped Heads") 
                         print("You lose!")
+                        investProfit = investProfit + gameProfit
                         balance = int(balance) - int(bet)
+                        bankroll = int(bankroll) + int(bet)
+                        gameProfit = int(bet) * float(percentOfStake)
+                        investedAmount = int(investedAmount) + int(gameProfit)
+                        investProfit = investProfit + gameProfit
+                        gameProfit = 0
+                        hcShare = int(bet) * 0.03
+                        if (int(hcamount) > 0):
+                                balance = int(balance)+ int(hcShare) * float(int(hcamount)/int(maxHC))
                         mode = None
                     else:
                         print ("The coin flipped Tails")
                         print("You win!")
-                        balance = int(balance) + int(bet)*2
+                        investProfit = investProfit - gameProfit
+                        balance = int(balance) + int(bet)*1.97
+                        bankroll = int(bankroll) - int(bet)
+                        gameProfit = int(bet) * float(percentOfStake)
+                        investedAmount = int(investedAmount) - int(gameProfit)
+                        investProfit = investProfit - gameProfit
+                        gameProfit = 0
+                        hcShare = int(bet) * 0.03
+                        if (int(hcamount) > 0):
+                                balance = int(balance)+ int(hcShare) * float(int(hcamount)/int(maxHC))
                         mode = None
         except(ValueError):
                 print("Refrain from using letters, or other symbols, when asked for a numerical value.")
                 mode = None
         #FAUCET GIVES YOU 1000 BITS
         if mode == "faucet":
-            balance = balance + 1000
-            mode = None
+                print("You have received 1000 bits")
+                balance = balance + 1000
+                mode = None
         #DISPLAYS BALANCE
         if mode == "balance":
             print(balance)
             print("Here's your house commission balance:")
             print(hcbalance)
             mode = None
-        #CHECK IF BALANCE IS ZERO AND SAY A MEAN MESSAGE
-        if balance < 0:
-            print("Haha you got raped by my 0% house edge bot")
-            
+                
         #BUY
         try:
                 if mode == "hcbuy":
@@ -128,5 +204,23 @@ while game_started == True:
         if mode == "exit":
                 sys.exit(0)
                 
+        if mode == "stats":
+                print("Bankroll: ",bankroll)
+                print("Bought House Commissions: ",currentHC)
+                print("")
+                mode = None
 
-        
+        if mode == "invest":
+                investmentAmount = input("How much do you want to invest? ")
+                investedAmount = int(investedAmount) + int(investmentAmount)
+                bankroll = int(bankroll) + int(investmentAmount)
+                balance = balance - investedAmount
+                print("Invested ",investmentAmount," to the bankroll")
+                mode = None
+        if mode == "stake":
+                percentOfStake = investedAmount/bankroll
+                print("Stake: ", investedAmount)
+                print("Percentage", percentOfStake * 100,"%")
+                print("Investment Profit: ",investProfit)
+                mode = None
+                
